104be549c8e617b2a6e74766089f0c75d7dd4051
mahbahig <mahbahig2@gmail.com>
2025-01-12 18:22:53 +0200

    Q2

---
+++ Assignment/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal struct Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"The name is {Name} and the age is {Age}";
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,21 +1,34 @@
 ﻿namespace Assignment
 {
     #region Q1
-    internal enum WeekDays
-    {
-        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
-    }
+    //internal enum WeekDays
+    //{
+    //    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    //}
     #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
             #region Q1
-            WeekDays weekDays;
-            for (int i = 0; i < 7; i++)
+            //WeekDays weekDays;
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    weekDays = (WeekDays)i;
+            //    Console.WriteLine(weekDays);
+            //}
+            #endregion
+
+            #region Q2
+            Person p1 = new Person("Mahmoud", 20);
+            Person p2 = new Person("Ahmed", 50);
+            Person p3 = new Person("Ibraim", 21);
+
+            Person[] people = { p1, p2, p3 };
+
+            foreach (Person p in people)
             {
-                weekDays = (WeekDays)i;
-                Console.WriteLine(weekDays);
+                Console.WriteLine(p.ToString());
             }
             #endregion
         }
