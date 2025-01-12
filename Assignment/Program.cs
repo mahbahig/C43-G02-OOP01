c4237cca35637e8b3e8ac165c7e56fd595994f8f
mahbahig <mahbahig2@gmail.com>
2025-01-12 17:59:10 +0200

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Assignment
 {
+    #region Q1
+    internal enum WeekDays
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            WeekDays weekDays;
+            for (int i = 0; i < 7; i++)
+            {
+                weekDays = (WeekDays)i;
+                Console.WriteLine(weekDays);
+            }
+            #endregion
         }
     }
 }
